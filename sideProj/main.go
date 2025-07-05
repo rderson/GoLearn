24c9e67eb0a10e805a9ef399f556863033cc9249
rderson <rasultorsh006@gmail.com>
2025-07-05 12:36:28 +0200

    working on skillMatch

---
+++ sideProj/main.go
@@ -0,0 +1,12 @@
+package main 
+
+import "fmt"
+
+// This is a simple Go program that prints "Bullshit" to the console.
+// It serves as a basic example of a Go application structure.
+// The main function is the entry point of the program.
+// To run this program, use the command: go run main.go
+
+func main()  {
+	fmt.Println("Bullshit")
+}
